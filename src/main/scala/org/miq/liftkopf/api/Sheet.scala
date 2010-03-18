723a924b60030589a54e1771104ea6e1918d0245
Mihael Vrbanec <miqster@gmx.de>
2010-03-18 22:35:52 +0100

    -set location header correctly -hold a list of sheets in memory

--- src/main/scala/org/miq/liftkopf/api/Sheet.scala
+++ src/main/scala/org/miq/liftkopf/api/Sheet.scala
@@ -1,25 +1,41 @@
 package org.miq.liftkopf.api
 
-import net.liftweb.common.{Failure, Full}
+import net.liftweb.common.{Full}
 import net.liftweb.http._
+import collection.mutable.ListBuffer
+
+class Sheet(val id: Int) {
+
+}
+
 
 object Sheet extends AcceptedContentProvider {
 
+  private val baseUrl = List("api", "sheet")
+  private val openSheets : ListBuffer[Sheet] = new ListBuffer[Sheet]
+
   def dispatch: LiftRules.DispatchPF = {
-    case r @ Req(List("api", "sheet"), _, PostRequest) => () => Full(createNewSheet(r))
+    case r @ Req(`baseUrl`, _, PostRequest) => () => Full(createNewSheet(r))
     // Invalid API request - route to our error handler
-    case Req(List("api", "sheet"), "", _) => () => Full(new MethodNotAllowedResponse)
+    case Req(`baseUrl`, "", _) => () => Full(new MethodNotAllowedResponse)
   }
 
   def createNewSheet(r: Req) : LiftResponse = {
     // TODO: create the sheet as an open sheet on the server and return the location
+    val newSheet = new Sheet(openSheets.size + 1)
+    openSheets + newSheet
     val bytes = "New open sheet created".getBytes("UTF-8")
+    // TODO: extract response into own type
     InMemoryResponse(
       bytes,
       (("Content-Length", bytes.length.toString) ::
         ("Content-Type", "text/plain; charset=utf-8") ::
-        ("Location", "somewhere...todo") :: S.getHeaders(Nil)),
+        ("Location", buildLocationUrl(r, newSheet.id)) :: S.getHeaders(Nil)),
       S.responseCookies,
       201)
+  }
+
+  private def buildLocationUrl(r: Req, sheetId: Int) : String = {
+    "http://" + r.request.serverName + ":" + r.request.serverPort + "/" + baseUrl.mkString("/") + "/" + sheetId
   }
 }

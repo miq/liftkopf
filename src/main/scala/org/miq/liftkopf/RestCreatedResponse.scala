eb17279500cd0401691c862bcece10abc9f6558e
Mihael Vrbanec <miqster@gmx.de>
2010-04-01 19:44:12 +0200

    -mixin HeaderStuff to improve code and reduce duplication

--- src/main/scala/org/miq/liftkopf/RestCreatedResponse.scala
+++ src/main/scala/org/miq/liftkopf/RestCreatedResponse.scala
@@ -1,15 +1,15 @@
 package org.miq.liftkopf
 
-import net.liftweb.http.{BasicResponse, LiftResponse, S, InMemoryResponse}
+import net.liftweb.http._
 
-class RestCreatedResponse(locationHeader: String, data: Array[Byte]) extends LiftResponse {
+class RestCreatedResponse(locationHeader: String, data: Array[Byte]) extends LiftResponse with HeaderStuff {
   override val toResponse: BasicResponse = new InMemoryResponse(
     data,
     ("Content-Length", data.length.toString)
       :: ("Content-Type", "text/plain; charset=utf-8")
       :: ("Location", locationHeader)
-      :: S.getHeaders(Nil),
-    S.responseCookies,
+      :: headers,
+    cookies,
     201)
 }
 

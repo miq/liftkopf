5c416859a6d4efe98d108b294dd92283e0ee06e0
Mihael Vrbanec <miqster@gmx.de>
2010-06-06 15:49:10 +0200

    implement point calculation for simple wins of either party

--- src/main/scala/org/miq/liftkopf/api/Deal.scala
+++ src/main/scala/org/miq/liftkopf/api/Deal.scala
@@ -5,6 +5,8 @@ case class Deal(gameType: String, score: Int, actions: List[Actions]) {
     println("score: " + score)
     println("actions count: " + actions.size)
     // TODO calculate the new standings
-    new Standing(actions.map(a => if (a.party == "re") 1 else -1))
+    val winner =  if (score > 120) "re" else "contra"
+    val points = if (winner == "re") 1 else -2
+    new Standing(actions.map(a => if (a.party == "re") points else -points))
   }
 }
--- src/test/scala/org/miq/liftkopf/api/DealTest.scala
+++ src/test/scala/org/miq/liftkopf/api/DealTest.scala
@@ -5,9 +5,10 @@ import org.junit.Test
 
 class DealTest extends AssertionsForJUnit {
 
-  @Test def calculateScoreWithoutExtraPoints() {
+  @Test def calculateSimpleWins() {
     val noSpecialActions = List(new Actions("re"), new Actions("contra"), new Actions("contra"), new Actions("re"))
     assert(new Standing(List(1, -1, -1, 1)) === new Deal("normal", 125, noSpecialActions).result)
+    assert(new Standing(List(-2, 2, 2, -2)) === new Deal("normal", 101, noSpecialActions).result)
   }
 
 }

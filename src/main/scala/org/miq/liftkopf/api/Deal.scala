10040571ce8a17f7cc27d42d803b6c9bffc070ce
Mihael Vrbanec <miqster@gmx.de>
2010-06-06 16:20:58 +0200

    implement point calculation for considerable wins (no 90, no 60, no 30 and shutout)

--- src/main/scala/org/miq/liftkopf/api/Deal.scala
+++ src/main/scala/org/miq/liftkopf/api/Deal.scala
@@ -6,7 +6,27 @@ case class Deal(gameType: String, score: Int, actions: List[Actions]) {
     println("actions count: " + actions.size)
     // TODO calculate the new standings
     val winner =  if (score > 120) "re" else "contra"
-    val points = if (winner == "re") 1 else -2
+    var (points, increment) = if (winner == "re") (1, 1) else (-2, -1)
+    // additional points for re winning steps
+    var tempScore = 150
+    while (score > tempScore && winner == "re") {
+      points += increment
+      tempScore += 30
+    }
+    // for shutting out contra
+    if (score == 240 && winner == "re") {
+      points += increment
+    }
+    // additional points for contra winning steps
+    var reLosingScore = 90
+    while (score < reLosingScore && winner == "contra") {
+      points += increment
+      reLosingScore -= 30
+    }
+    // for shutting out contra
+    if (score == 0 && winner == "contra") {
+      points += increment
+    }
     new Standing(actions.map(a => if (a.party == "re") points else -points))
   }
 }
--- src/test/scala/org/miq/liftkopf/api/DealTest.scala
+++ src/test/scala/org/miq/liftkopf/api/DealTest.scala
@@ -4,11 +4,25 @@ import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Test
 
 class DealTest extends AssertionsForJUnit {
+  private val noSpecialActions = List(new Actions("re"), new Actions("contra"), new Actions("contra"), new Actions("re"))
 
   @Test def calculateSimpleWins() {
-    val noSpecialActions = List(new Actions("re"), new Actions("contra"), new Actions("contra"), new Actions("re"))
     assert(new Standing(List(1, -1, -1, 1)) === new Deal("normal", 125, noSpecialActions).result)
-    assert(new Standing(List(-2, 2, 2, -2)) === new Deal("normal", 101, noSpecialActions).result)
+  }
+
+  @Test def calculateScoreTie() {
+    assert(new Standing(List(-2, 2, 2, -2)) === new Deal("normal", 120, noSpecialActions).result)
+  }
+
+  @Test def calculateConsiderableWins() {
+    assert(new Standing(List(2, -2, -2, 2)) === new Deal("normal", 151, noSpecialActions).result)
+    assert(new Standing(List(-3, 3, 3, -3)) === new Deal("normal", 89, noSpecialActions).result)
+    assert(new Standing(List(3, -3, -3, 3)) === new Deal("normal", 181, noSpecialActions).result)
+    assert(new Standing(List(-4, 4, 4, -4)) === new Deal("normal", 59, noSpecialActions).result)
+    assert(new Standing(List(4, -4, -4, 4)) === new Deal("normal", 211, noSpecialActions).result)
+    assert(new Standing(List(-5, 5, 5, -5)) === new Deal("normal", 29, noSpecialActions).result)
+    assert(new Standing(List(5, -5, -5, 5)) === new Deal("normal", 240, noSpecialActions).result)
+    assert(new Standing(List(-6, 6, 6, -6)) === new Deal("normal", 0, noSpecialActions).result)
   }
 
 }

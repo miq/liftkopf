6a2a168d28bb11bdba88f36dffbbd4d9679e11e1
Mihael Vrbanec <miqster@gmx.de>
2010-06-09 09:00:37 +0200

    corrected the order of the assertions

--- src/test/scala/org/miq/liftkopf/api/DealTest.scala
+++ src/test/scala/org/miq/liftkopf/api/DealTest.scala
@@ -9,36 +9,36 @@ class DealTest extends AssertionsForJUnit {
   private val noSpecialActions = List(reAction, contraAction, contraAction, reAction)
 
   @Test def calculateSimpleWins() {
-    assert(new Standing(List(1, -1, -1, 1)) === new Deal("normal", 125, noSpecialActions).result)
+    assert(new Deal("normal", 125, noSpecialActions).result === new Standing(List(1, -1, -1, 1)))
   }
 
   @Test def calculateScoreTie() {
-    assert(new Standing(List(-2, 2, 2, -2)) === new Deal("normal", 120, noSpecialActions).result)
+    assert(new Deal("normal", 120, noSpecialActions).result === new Standing(List(-2, 2, 2, -2)))
   }
 
   @Test def calculateConsiderableWins() {
-    assert(new Standing(List(2, -2, -2, 2)) === new Deal("normal", 151, noSpecialActions).result)
-    assert(new Standing(List(-3, 3, 3, -3)) === new Deal("normal", 89, noSpecialActions).result)
-    assert(new Standing(List(3, -3, -3, 3)) === new Deal("normal", 181, noSpecialActions).result)
-    assert(new Standing(List(-4, 4, 4, -4)) === new Deal("normal", 59, noSpecialActions).result)
-    assert(new Standing(List(4, -4, -4, 4)) === new Deal("normal", 211, noSpecialActions).result)
-    assert(new Standing(List(-5, 5, 5, -5)) === new Deal("normal", 29, noSpecialActions).result)
-    assert(new Standing(List(5, -5, -5, 5)) === new Deal("normal", 240, noSpecialActions).result)
-    assert(new Standing(List(-6, 6, 6, -6)) === new Deal("normal", 0, noSpecialActions).result)
+    assert(new Deal("normal", 151, noSpecialActions).result === new Standing(List(2, -2, -2, 2)))
+    assert(new Deal("normal", 89, noSpecialActions).result === new Standing(List(-3, 3, 3, -3)))
+    assert(new Deal("normal", 181, noSpecialActions).result === new Standing(List(3, -3, -3, 3)))
+    assert(new Deal("normal", 59, noSpecialActions).result === new Standing(List(-4, 4, 4, -4)))
+    assert(new Deal("normal", 211, noSpecialActions).result === new Standing(List(4, -4, -4, 4)))
+    assert(new Deal("normal", 29, noSpecialActions).result === new Standing(List(-5, 5, 5, -5)))
+    assert(new Deal("normal", 240, noSpecialActions).result === new Standing(List(5, -5, -5, 5)))
+    assert(new Deal("normal", 0, noSpecialActions).result === new Standing(List(-6, 6, 6, -6)))
   }
 
   @Test def calculateAnnouncedWins() {
     val reWinActions = List(reAction, new Actions(Deal.Re, 120), contraAction, contraAction)
-    assert(new Standing(List(4, 4, -4, -4)) === new Deal("normal", 151, reWinActions).result)
-    assert(new Standing(List(6, 6, -6, -6)) === new Deal("normal", 181, reWinActions).result)
+    assert(new Deal("normal", 151, reWinActions).result === new Standing(List(4, 4, -4, -4)))
+    assert(new Deal("normal", 181, reWinActions).result === new Standing(List(6, 6, -6, -6)))
     val contraWinActions = List(contraAction, reAction, new Actions(Deal.Contra, 120), reAction)
-    assert(new Standing(List(4, -4, 4, -4)) === new Deal("normal", 119, contraWinActions).result)
-    assert(new Standing(List(10, -10, 10, -10)) === new Deal("normal", 29, contraWinActions).result)
+    assert(new Deal("normal", 119, contraWinActions).result === new Standing(List(4, -4, 4, -4)))
+    assert(new Deal("normal", 29, contraWinActions).result === new Standing(List(10, -10, 10, -10)))
   }
 
   @Test def calculateLossBecauseAnnouncement() {
     val reLossActions = List(new Actions(Deal.Re, 90), contraAction, reAction, contraAction)
-    assert(new Standing(List(-8, 8, -8, 8)) === new Deal("normal", 150, reLossActions).result)
+    assert(new Deal("normal", 150, reLossActions).result === new Standing(List(-8, 8, -8, 8)))
   }
 
 }

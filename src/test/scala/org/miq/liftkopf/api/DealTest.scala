e0b350e8088b46e7aa3f3ed46a5fe7639b69b713
Mihael Vrbanec <miqster@gmx.de>
2010-06-25 00:07:52 +0200

    implement extra point calculation and tests for it

--- src/test/scala/org/miq/liftkopf/api/DealTest.scala
+++ src/test/scala/org/miq/liftkopf/api/DealTest.scala
@@ -52,10 +52,79 @@ class DealTest extends ShouldMatchersForJUnit {
     new Deal(90, contraLossActions).result should be (new Standing(List(6, 6, -6, -6)))
   }
 
-  @Test def calculateSoloWins() {
+  @Test def calculateSoloGames() {
     val soloActions = List(contraAction, contraAction, reAction, contraAction)
     new Deal(Deal.JackSoloType, 130, soloActions).result should be (new Standing(List(-1, -1, 3, -1)))
+    new Deal(Deal.QuietSoloType, 200, soloActions).result should be (new Standing(List(-3, -3, 9, -3)))
     new Deal(Deal.QueenSoloType, 119, soloActions).result should be (new Standing(List(1, 1, -3, 1)))
+    new Deal(Deal.ColorSoloType, 88, soloActions).result should be (new Standing(List(2, 2, -6, 2)))
   }
 
+  @Test def calculateSoloBids() {
+    val soloBid = List(contraAction, new Actions(Deal.Re, 90), contraAction, contraAction)
+    new Deal(Deal.QueenSoloType, 154, soloBid).result should be (new Standing(List(-6, 18, -6, -6)))
+    new Deal(Deal.QueenSoloType, 147, soloBid).result should be (new Standing(List(6, -18, 6, 6)))
+  }
+
+  @Test def calculateFoxExtras() {
+    val foxForRe = List(contraAction,
+      new Actions(Deal.Re, 120, 1, 0, "", 0, false, false, false),
+      reAction,
+      new Actions(Deal.Contra, 0, 0, 1, "", 0, false, false, false))
+    new Deal(121, foxForRe).result should be (new Standing(List(-3, 3, 3, -3)))
+    val foxForEach = List(
+      new Actions(Deal.Contra, 0, 1, 0, "", 0, false, false, false),
+      new Actions(Deal.Re, 120, 1, 1, "", 0, false, false, false),
+      reAction,
+      new Actions(Deal.Contra, 0, 0, 1, "", 0, false, false, false))
+    new Deal(121, foxForEach).result should be (new Standing(List(-2, 2, 2, -2)))
+    val twoFoxesForContra = List(
+      contraAction,
+      new Actions(Deal.Re, 120, 0, 1, "", 0, false, false, false),
+      new Actions(Deal.Re, 0, 0, 1, "", 0, false, false, false),
+      new Actions(Deal.Contra, 0, 2, 0, "", 0, false, false, false))
+    new Deal(121, twoFoxesForContra).result should be (new Standing(List(0, 0, 0, 0)))
+  }
+
+  @Test def calculateCharlyExtras() {
+    val charlyPoint = List(contraAction,
+      new Actions(Deal.Re, 0, 0, 0, Actions.CharlyPoint, 0, false, false, false),
+      reAction,
+      contraAction)
+    new Deal(128, charlyPoint).result should be (new Standing(List(-2, 2, 2, -2)))
+    val charlyCaught = List(contraAction,
+      new Actions(Deal.Re, 0, 0, 0, Actions.CharlyCaught, 0, false, false, false),
+      reAction,
+      new Actions(Deal.Contra, 0, 0, 0, Actions.CharlyLost, 0, false, false, false))
+    new Deal(128, charlyCaught).result should be (new Standing(List(-2, 2, 2, -2)))
+    val charlyLost = List(
+      new Actions(Deal.Contra, 0, 0, 0, Actions.CharlyCaught, 0, false, false, false),
+      new Actions(Deal.Re, 0, 0, 0, Actions.CharlyLost, 0, false, false, false),
+      reAction,
+      contraAction)
+    new Deal(128, charlyLost).result should be (new Standing(List(0, 0, 0, 0)))
+    val charlyPointCaught = List(contraAction,
+      new Actions(Deal.Re, 0, 0, 0, Actions.CharlyPointCaught, 0, false, false, false),
+      reAction,
+      new Actions(Deal.Contra, 0, 0, 0, Actions.CharlyLost, 0, false, false, false))
+    new Deal(128, charlyPointCaught).result should be (new Standing(List(-3, 3, 3, -3)))
+  }
+
+  @Test def calculateDoubleHeadExtras() {
+    val oneForRe = List(contraAction,
+      new Actions(Deal.Re, 0, 0, 0, "", 1, false, false, false),
+      reAction,
+      contraAction)
+    new Deal(128, oneForRe).result should be (new Standing(List(-2, 2, 2, -2)))
+    val twoForRe = List(contraAction,
+      new Actions(Deal.Re, 0, 0, 0, "", 2, false, false, false),
+      reAction,
+      contraAction)
+    new Deal(128, twoForRe).result should be (new Standing(List(-3, 3, 3, -3)))
+    val oneForEach = List(contraAction,
+      new Actions(Deal.Re, 0, 0, 0, "", 1, false, false, false),
+      reAction,
+      new Actions(Deal.Contra, 0, 0, 0, "", 1, false, false, false))
+    new Deal(128, oneForEach).result should be (new Standing(List(-1, 1, 1, -1)))
+  }
 }

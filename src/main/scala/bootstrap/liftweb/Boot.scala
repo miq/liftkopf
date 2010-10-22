41b06a7e9725402cd7ffa6ce28e7b183a542838d
Mihael Vrbanec <miqster@gmx.de>
2010-10-22 19:48:03 +0200

    update project to lift 2.1 final and switch to the h2 database

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -2,15 +2,11 @@ package bootstrap.liftweb
 
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.sitemap._
-import _root_.net.liftweb.sitemap.Loc._
-import Helpers._
-import _root_.net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, ConnectionIdentifier}
-import _root_.java.sql.{Connection, DriverManager}
 import _root_.org.miq.model._
-import _root_.javax.servlet.http.{HttpServletRequest}
 import net.liftweb.common._
 import net.liftweb.http._
 import org.miq.liftkopf.api.{Sheet, PlayerOverview}
+import net.liftweb.mapper._
 
 /**
  * A class that's instantiated early and run.  It allows the application
@@ -19,7 +15,12 @@ import org.miq.liftkopf.api.{Sheet, PlayerOverview}
 class Boot {
   def boot {
     if (!DB.jndiJdbcConnAvailable_?) {
-      DB.defineConnectionManager(DefaultConnectionIdentifier, DBVendor)
+      val vendor = new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+          Props.get("db.url") openOr "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+          Props.get("db.user"),
+          Props.get("db.password"))
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
     }
     // where to search snippet
     LiftRules.addToPackages("org.miq")
@@ -47,59 +48,4 @@ class Boot {
         case _ if S.getDocType._1 => S.getDocType._2
         case _ => Full(DocType.xhtmlStrict)
       }
-}
-
-/**
- * Database connection calculation
- */
-object DBVendor extends ConnectionManager {
-  private var pool: List[Connection] = Nil
-  private var poolSize = 0
-  private val maxPoolSize = 4
-
-  private def createOne: Box[Connection] = try {
-    val driverName: String = Props.get("db.driver") openOr
-            "org.apache.derby.jdbc.EmbeddedDriver"
-    val dbUrl: String = Props.get("db.url") openOr
-            "jdbc:derby:liftkopfdb;create=true"
-    Class.forName(driverName)
-    val dm = (Props.get("db.user"), Props.get("db.password")) match {
-      case (Full(user), Full(pwd)) => DriverManager.getConnection(dbUrl, user, pwd)
-      case _ => DriverManager.getConnection(dbUrl)
-    }
-    Full(dm)
-  } catch {
-    case e: Exception => e.printStackTrace; Empty
-  }
-
-  def newConnection(name: ConnectionIdentifier): Box[Connection] =
-    synchronized {
-      pool match {
-        case Nil if poolSize < maxPoolSize =>
-          val ret = createOne
-          poolSize = poolSize + 1
-          ret.foreach(c => pool = c :: pool)
-          ret
-
-        case Nil => wait(1000L); newConnection(name)
-        case x :: xs => try {
-          x.setAutoCommit(false)
-          Full(x)
-        } catch {
-          case e => try {
-            pool = xs
-            poolSize = poolSize - 1
-            x.close
-            newConnection(name)
-          } catch {
-            case e => newConnection(name)
-          }
-        }
-      }
-    }
-
-  def releaseConnection(conn: Connection): Unit = synchronized {
-    pool = conn :: pool
-    notify
-  }
 }

aa0a815c3873bcac424b0596deca101939f1ffbe
Mihael Koep <miqster@gmx.de>
2011-05-15 20:32:57 +0200

    update to Scala 2.8.1 and Lift 2.3

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -24,7 +24,7 @@ class Boot {
     }
     // where to search snippet
     LiftRules.addToPackages("org.miq")
-    Schemifier.schemify(true, Log.infoF _, User)
+    Schemifier.schemify(true, Schemifier.infoF _, User)
     // Build SiteMap
     val entries = Menu(Loc("Home", List("index"), S.?("Home"))) :: Menu(Loc("Statistics", List("statistics"), S.?("Statistics"))) :: User.sitemap
     LiftRules.setSiteMap(SiteMap(entries: _*))
--- src/main/scala/org/miq/liftkopf/api/Sheet.scala
+++ src/main/scala/org/miq/liftkopf/api/Sheet.scala
@@ -5,8 +5,8 @@ import collection.mutable.ListBuffer
 import net.liftweb.json.JsonAST.JValue
 import net.liftweb.common.Full
 import org.miq.liftkopf.{LiftkopfRest, RestCreatedResponse}
-import net.liftweb.json.JsonAST
 import net.liftweb.json.JsonDSL._
+import net.liftweb.json.{Printer, JsonAST}
 
 class Sheet(val id: Int, val location: String, val group: String, val playerIds: List[Int]) {
   private val deals : ListBuffer[Deal] = new ListBuffer[Deal]
@@ -44,14 +44,14 @@ object Sheet extends LiftkopfRest {
     val newSheet = new Sheet(openSheets.size + 1, newSheetRequest.location, newSheetRequest.group, playerIds.map(_.toInt))
     openSheets + newSheet
     val jsonResponse = ("standing" -> newSheet.getStanding.scores)
-    RestCreatedResponse(buildLocationUrl(r, newSheet.id), "application/json", compact(JsonAST.render(jsonResponse)))
+    RestCreatedResponse(buildLocationUrl(r, newSheet.id), "application/json", Printer.compact(JsonAST.render(jsonResponse)))
   }
 
   private def addDealTo(sheetId: Int, json: JValue, r: Req) : LiftResponse = {
     println("adding deal to sheet" + sheetId)
     val deal = json.extract[Deal]
     val jsonResponse = ("standing" -> openSheets(sheetId - 1).addDeal(deal).scores)
-    RestCreatedResponse(buildLocationUrl(r, openSheets(sheetId - 1).id), "application/json", compact(JsonAST.render(jsonResponse)))
+    RestCreatedResponse(buildLocationUrl(r, openSheets(sheetId - 1).id), "application/json", Printer.compact(JsonAST.render(jsonResponse)))
   }
 }
 
